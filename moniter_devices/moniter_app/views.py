a36fe2eefbd832c3670e1de0b06eb44c45ec4c22
Shakrah Yves <7shakrahs@gmail.com>
2018-07-13 17:29:52 +0200

    do business login in view functions

--- moniter_devices/moniter_app/views.py
+++ moniter_devices/moniter_app/views.py
@@ -1,20 +1,40 @@
 from django.shortcuts import get_object_or_404, render
 from django.db.models import Count
+import datetime
 
 from .models import Occurance
+from operator import itemgetter
+
+def percentage_change(old, new):
+    if old == 0:
+        return 'n/a'
+    return "{:.1f} %".format((new - old) / old * 100)
+
+def get_count_by_day(date):
+    l = (Occurance.objects
+        .values('device_id')
+        .filter(date=date)
+        .annotate(dcount=Count('device_id')))
+    return {elem['device_id']: elem['dcount']  for elem in l}
 
 def index(request):
-    occurances = Occurance.objects.filter(status='online', device_id='bb10684d')[:30]
+    occurances = Occurance.objects.values('device_id').distinct()
     return render(request, 'moniter_app/index.html', {'occurances': occurances})
 
-def date(request, date):
-    #occurances2 = Occurance.objects.order_by('datetime')[:10]
-    occurances = Occurance.objects.all()
-    return render(request, 'moniter_app/date.html', {'occurances': occurances})
+def date(request, date_string):
+    date = datetime.datetime.strptime(date_string, '%Y-%m-%d').date()
+    selected_date_data_set = list(get_count_by_day(date).items())
+    selected_date_data_set.sort(key=itemgetter(1), reverse=True)
 
-def device(request, device_id, status):
-    #todays date -30 time delta?
-    id = device_id
-    stat = status
-    occurances = Occurance.objects.values('date').filter(device_id=device_id,status=status).annotate(dcount=Count('date'))
+    delta_date_data_set = get_count_by_day(date - datetime.timedelta(days=7))
+    popular = [{'device_id': device_id,
+                'count': count,
+                'change': percentage_change(delta_date_data_set.get(device_id, 0), count),
+                'old_count': delta_date_data_set.get(device_id, 0)}
+               for device_id, count in selected_date_data_set[:10]]
+    return render(request, 'moniter_app/date.html', {'popular': popular})
+
+def device(request, device_type, status):
+    device = device_type
+    occurances = Occurance.objects.values('date').filter(device_type=device_type,status=status).annotate(dcount=Count('date'))
     return render(request, 'moniter_app/device.html', {'occurances': occurances})
