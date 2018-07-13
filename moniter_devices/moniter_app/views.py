0f0a8f345f4edc60e7d1a97d000bf2e84bdfa6db
Shakrah Yves <7shakrahs@gmail.com>
2018-07-13 14:10:53 +0200

    view by device mvp

--- moniter_devices/moniter_app/views.py
+++ moniter_devices/moniter_app/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.db.models import Count
 
-from django.http import HttpResponse
-
+from .models import Occurance
 
 def index(request):
-    return HttpResponse("Moniter Devices.")
+    occurances = Occurance.objects.filter(status='online', device_id='bb10684d')[:30]
+    return render(request, 'moniter_app/index.html', {'occurances': occurances})
 
-def date(request, datetime):
-    return HttpResponse("You're looking at question %s." % datetime)
+def date(request, date):
+    #occurances2 = Occurance.objects.order_by('datetime')[:10]
+    occurances = Occurance.objects.all()
+    return render(request, 'moniter_app/date.html', {'occurances': occurances})
 
-def device(request, device_type, status, ):
-    response = "You're looking at device by status over 30 days %s."
-    return HttpResponse(response % device_type % status)
+def device(request, device_id, status):
+    #todays date -30 time delta?
+    id = device_id
+    stat = status
+    occurances = Occurance.objects.values('date').filter(device_id=device_id,status=status).annotate(dcount=Count('date'))
+    return render(request, 'moniter_app/device.html', {'occurances': occurances})
