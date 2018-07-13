9b3d4ce7837478d3e2a1df8c80ba5c4093dfc1cc
Shakrah Yves <7shakrahs@gmail.com>
2018-07-13 17:29:11 +0200

    update urls with correct types

--- moniter_devices/moniter_app/urls.py
+++ moniter_devices/moniter_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # ex: /moniter_app/device/'bb10684d'/results
-    path('device/<str:device_id>/<str:status>/', views.device, name='device'),
-    # ex: /moniter_app/15/results/
-    path('date/<int:datetime>/', views.date, name='date'),
+    # ex: /moniter_app/gateway/online
+    path('device/<str:device_type>/<str:status>/', views.device, name='device'),
+    # ex: /moniter_app/date/2017-05-16
+    path('date/<str:date_string>/', views.date, name='date'),
 ]
