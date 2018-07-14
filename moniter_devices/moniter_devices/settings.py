5eccbc82938190e252df9e4149118eea5c6ac9ed
Shakrah Yves <7shakrahs@gmail.com>
2018-07-14 11:56:44 +0200

    serve the files in dev

--- moniter_devices/moniter_devices/settings.py
+++ moniter_devices/moniter_devices/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.staticfiles',
     'moniter_app.apps.MoniterAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
