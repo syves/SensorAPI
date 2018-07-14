0d8736d2f44a9f65fefc912dfa4281a8a4b36139
Shakrah Yves <7shakrahs@gmail.com>
2018-07-14 11:51:56 +0200

    add support for adding styles-wip

--- moniter_devices/moniter_app/urls.py
+++ moniter_devices/moniter_app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+#from django.conf import settings
+#from django.conf.urls.static import static
 
 from . import views
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path('device/<str:device_type>/<str:status>/', views.device, name='device'),
     # ex: /moniter_app/date/2017-05-16
     path('date/<str:date_string>/', views.date, name='date'),
-]
+] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
