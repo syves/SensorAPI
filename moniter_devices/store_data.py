5da79f5e7cc643cde2f59846b39331c365204883
Shakrah Yves <7shakrahs@gmail.com>
2018-07-16 10:50:27 +0200

    store data script works

--- moniter_devices/store_data.py
+++ moniter_devices/store_data.py
@@ -2,29 +2,22 @@
 import os
 import django
 import sys
-import readline # optional, will allow Up/Down/History in the console
-import code
 import json
+from django.db import models
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moniter_devices.settings")
 django.setup()
 from moniter_app.models import Occurance
 
-variables = globals().copy()
-variables.update(locals())
-shell = code.InteractiveConsole(variables)
-#shell.interact()
+from django.apps import apps
+ap=apps.get_model('moniter_app','Occurance')
 
 cleaned_data_dir = "/Users/syves/github.com/syves/Assignment/moniter_devices/processed/"
 cleaned_data = "/Users/syves/github.com/syves/Assignment/moniter_devices/processed/clean_data.json"
 
-mydata = "foo"
-
 for file in os.listdir(cleaned_data_dir):
-
     with open(cleaned_data, 'r') as input:
         data =  json.load(input)
         for tup in data:
             print(tup)
-            #Occurance.from_tuple(*(tup)); s.save()
-#save to the database somehow.
+            s = Occurance.from_tuple(*(tup)); s.save()
